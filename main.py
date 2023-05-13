849b9d2bfa3b888c425e0b2e4edcc2481d4d3173
nowalor <no@walor.io>
2023-05-13 19:59:06 +0200

    feat: db connected and new endpoints added

--- main.py
+++ main.py
@@ -1,13 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import uvicorn
-
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+import models
+from pydantic import BaseModel
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+
+class User(BaseModel):
+    email: str
+    password: str
+
+
+class Note(BaseModel):
+    user_id: int
+    text: str
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    except:
+        print("Some exception")
+    finally:
+        db.close()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_api(db: Session = Depends(get_db)):
+    return db.query(models.Note).all()
+
+
+@app.post("/users")
+def create_user(user: User, db: Session = Depends(get_db)):
+    user_model = models.User()
+
+    user_model.email = user.email
+    user_model.password = user.password
+
+    db.add(user_model)
+    db.commit()
+
+
+@app.post("/notes")
+def create_note(note: Note, db: Session = Depends(get_db)):
+    note_model = models.Note()
+
+    note_model.user_id = note.user_id
+    note_model.text = note.text
+
+    db.add(note_model)
+    db.commit()
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,4 +1,24 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-from .db import DB
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+    notes = relationship("Note", back_populates="user")
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    text = Column(String)
+
+    user = relationship("User", back_populates="notes")
